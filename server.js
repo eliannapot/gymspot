f8b082e0875b7fabeab62d78f18ab00ab7fb8bdb
eliannapot <up1072682@upnet.gr>
2023-05-24 19:47:30 +0300

    routing

--- server.js
+++ server.js
@@ -17,26 +17,6 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public')); // serve static files
 app.use('/', routes);
 
-// //Routing
-// app.get('/', (req, res) => {
-//     res.render('index', { title: 'Home', javascript: 'home.js' });
-// });
-
-// app.get('/services', (req, res) => {
-//     res.render('services', { title: 'Services', javascript: 'services.js' });
-// });
-
-// app.get('/programs', (req, res) => {
-//     res.render('programs', { title: 'Programs', javascript: 'programs.js' });
-// });
-
-// app.get('/admin', (req, res) => {
-//     res.render('admin', { title: 'Admin', javascript: 'admin.js' });
-// });
-
-// app.get('*', (req, res) => {
-//     res.json('Page not found');
-// });
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
