4e25558c28965935f77076fd2329e5615ecc9e60
eliannapot <up1072682@upnet.gr>
2023-05-28 21:09:07 +0300

    deleted folder with previous ver

--- trash/new_programs.js
+++
@@ -1,5 +0,0 @@
-function addNewProgram(program, desc, difficulty, time) {
-    let box = document.querySelector(".program-container")
-    let newLi = document.createElement(".program");
-    box.appendChild(newLi)
-}
